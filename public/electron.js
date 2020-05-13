52425ed747072a590b4d9835a2983fbcf46ccd53
Stuart Thompson <stuart@stuartthompson.net>
2020-05-13 08:17:02 -0700

    Allows running locally as well as distribution.

--- public/electron.js
+++ public/electron.js
@@ -6,7 +6,11 @@ function createWindow() {
   win = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
-  win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
+  if (process.env.NODE_ENV === "local") {
+    win.loadURL('http://localhost:3000');
+  } else {
+    win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
+  }
 }
 
 app.on("ready", createWindow);
