18f2748af1c9730dca9c388db37a08de40b78023
Stuart Thompson <stuart@stuartthompson.net>
2021-02-19 18:42:52 -0800

    Removes admin connection and adds ad-hoc text messaging.

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -7,22 +7,16 @@ import WSClientStatus from '../components/WSClientStatus';
 const HomeScreen = () => {
     const IP = "127.0.0.1";
     const CLIENT_PORT = "4000";
-    const ADMIN_PORT = "4001";
-
+    
     const [clientStatus, setClientStatus] = useState("None");
-    const [adminStatus, setAdminStatus] = useState("None");
+    const [textToSend, setTextToSend] = useState("");
 
     const clientConnInfo = {
         ip: IP,
         port: CLIENT_PORT
     };
-    const adminConnInfo = {
-        ip: IP,
-        port: ADMIN_PORT
-    }
-
+    
     const clientWS = useRef();
-    const adminWS = useRef();
 
     const onClientWebSocketOpen = e => {
         console.log(`[Client] Websocket opened.`);
@@ -42,24 +36,6 @@ const HomeScreen = () => {
         console.log(`[Client] Websocket closed.`);
     };
 
-    const onAdminWebSocketOpen = e => {
-        console.log(`[Admin] Websocket opened.`);
-    };
-
-    const onAdminWebSocketError = e => {
-        console.log("[Admin] Error: " + JSON.stringify(e));
-    };
-
-    const onAdminWebSocketMessage = e => {
-        console.log("[Admin] Message received.");
-        console.log(`[Admin] Received: ${e.data}`);
-        setAdminStatus(JSON.parse(e.data));
-    };
-
-    const onAdminWebSocketClose = () => {
-        console.log(`[Admin] Websocket closed.`);
-    };
-
     useEffect(() => {
         // Set up client websocket
         clientWS.current = new WebSocket(`ws://${clientConnInfo.ip}:${clientConnInfo.port}`)
@@ -67,32 +43,29 @@ const HomeScreen = () => {
         clientWS.current.onerror = onClientWebSocketError;
         clientWS.current.onmessage = onClientWebSocketMessage;
         clientWS.current.onclose = onClientWebSocketClose;
-
-        // Set up admin websocket
-        adminWS.current = new WebSocket(`ws://${adminConnInfo.ip}:${adminConnInfo.port}`)
-        adminWS.current.onopen = onAdminWebSocketOpen;
-        adminWS.current.onerror = onAdminWebSocketError;
-        adminWS.current.onmessage = onAdminWebSocketMessage;
-        adminWS.current.onclose = onAdminWebSocketClose;
-    }, [clientConnInfo.ip, clientConnInfo.port, adminConnInfo.ip, adminConnInfo.port]);
+    }, [clientConnInfo.ip, clientConnInfo.port]);
 
     const onSendHelloClicked = () => {
         clientWS.current.send("Hello!");
     }
 
-    const onSendShutdownClicked = () => {
-        adminWS.current.send("ShutdownServer");
+    const onSendTextClicked = () => {
+        clientWS.current.send(textToSend);
+    }
+
+    const onTextToSendChanged = event => {
+        console.log(`text: ${event.target.value}`);
+        setTextToSend(event.target.value);
     }
 
     return (
         <>
             <h1>Regular Client</h1>
             <WSClientStatus connectionInfo={clientConnInfo} status={clientStatus} />
+            <input type="text" onChange={onTextToSendChanged} value={textToSend} />
+            <input type="button" value="Send" onClick={onSendTextClicked} />
+            <br/><br/>
             <input type="button" value="Send Hello" onClick={onSendHelloClicked} />
-
-            <h1>Admin Client</h1>
-            <WSClientStatus connectionInfo={adminConnInfo} status={adminStatus} />
-            <input type="button" value="Send Shutdown" onClick={onSendShutdownClicked} />
         </>
     );
 };
